6ddeda2322e44277244c06723bbb1b72ffc767fb
Benjamin Bouachour <benjaminbouachour@iMac-de-Benjamin.local>
2016-03-10 10:00:09 +0100

    framework proj

---
+++ Franz/Franz/Franz.h
@@ -0,0 +1,19 @@
+//
+//  Franz.h
+//  Franz
+//
+//  Created by Benjamin Bouachour on 10/03/16.
+//  Copyright © 2016 BioSerenity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Franz.
+FOUNDATION_EXPORT double FranzVersionNumber;
+
+//! Project version string for Franz.
+FOUNDATION_EXPORT const unsigned char FranzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Franz/PublicHeader.h>
+
+
